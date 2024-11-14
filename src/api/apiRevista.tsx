3c459bc679d03b0a6d9f367b9d68815e7e6c4bb5
Alan Rodas Bonjour <alanrodas@gmail.com>
2024-11-14 17:20:07 -0300

    chore: Adding support for docker and docker compose

--- src/api/apiRevista.tsx
+++ src/api/apiRevista.tsx
@@ -1,22 +1,21 @@
 import axios from 'axios';
 
 const apiRevista = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: window.location.protocol + "//" + window.location.host + '/api'
 });
-
 
 apiRevista.interceptors.request.use((config) => {
     const accessToken = localStorage.getItem('accessToken');
-    
-    
+
+
     if (accessToken && config.headers && !(config.url?.includes('/login') ||
-                                           !config.url?.includes('/home') || 
+                                           !config.url?.includes('/home') ||
                                            !config.url?.includes('/uploads') ||
                                            !config.url?.includes('/revistas/uplods') ||
                                            !config.url?.includes('/registrarse'))) {
         config.headers.Authorization = `Bearer ${accessToken}`;
     }
-    
+
     return config;
 }, (error) => {
     console.error("✉️ ", error);
