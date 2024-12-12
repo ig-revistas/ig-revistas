17881841e82e96eb8d7e1c4447d95035a3693c44
eze6532 <ezequuielrumi15@gmail.com>
2024-12-12 16:04:18 -0300

    Actualiza MenuGestionRevista con nuevas funcionalidades

--- src/componentes/navbar/menuGestionRevista/MenuGestionRevista.tsx
+++ src/componentes/navbar/menuGestionRevista/MenuGestionRevista.tsx
@@ -7,8 +7,8 @@ const MenuGestionRevista: React.FC = () => {
 
   const handleNuevaRevista = () => navigate('/new_revista');
   const handleListaRevistas = () => navigate('/revistas');
-  const handlePendientes = () => navigate('/reservasPendientes'); 
-  const handleAprobadas = () => navigate('/reservasAprobadas'); 
+  const handlePendientes = () => navigate('/reservasPendientes');
+  const handleAprobadas = () => navigate('/reservasAprobadas');
   const handleReporteRevistas = () => navigate('/reporte-revistas');
 
   return (
@@ -69,7 +69,7 @@ const MenuGestionRevista: React.FC = () => {
             <line x1="3" y1="12" x2="3.01" y2="12"></line>
             <line x1="3" y1="18" x2="3.01" y2="18"></line>
           </svg>
-          <p>Reservas-Pendiente</p>
+          <p>Reservas Pendientes</p>
         </li>
         <li className="menuGestionRevistaItem" onClick={handleAprobadas}>
           <svg
@@ -82,14 +82,13 @@ const MenuGestionRevista: React.FC = () => {
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-            <line x1="8" y1="6" x2="21" y2="6"></line>
-            <line x1="8" y1="12" x2="21" y2="12"></line>
-            <line x1="8" y1="18" x2="21" y2="18"></line>
-            <line x1="3" y1="6" x2="3.01" y2="6"></line>
-            <line x1="3" y1="12" x2="3.01" y2="12"></line>
-            <line x1="3" y1="18" x2="3.01" y2="18"></line>
+            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+            <polyline points="14 2 14 8 20 8"></polyline>
+            <line x1="16" y1="13" x2="8" y2="13"></line>
+            <line x1="16" y1="17" x2="8" y2="17"></line>
+            <line x1="10" y1="9" x2="8" y2="9"></line>
           </svg>
-          <p>Reservas-Aprobada</p>
+          <p>Reservas Aprobadas</p>
         </li>
         <li className="menuGestionRevistaItem" onClick={handleReporteRevistas}>
           <svg
